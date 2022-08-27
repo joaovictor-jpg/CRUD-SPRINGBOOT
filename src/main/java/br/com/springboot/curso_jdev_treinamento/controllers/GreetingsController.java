b4c36cabee03aed4248c5e068209a37e75878d59
joao victor <joaovictormdasilva676@gmail.com>
2022-08-26 21:32:18 -0300

    Criando o model e tabela no banco e Criando o Repository com Spring Data

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,11 +1,13 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 /**
  *
@@ -13,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired/*INJEÇÃO DE DEPENDENCIA*/
+	private UsuarioRepository usuarioRepository;
+	
     /**
      *
      * @param name the name to greet
      * @return greeting text
      */
-    @RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Curso Spring Boot API: " + name + "!";
+    @GetMapping(value = "/mostranome/{name}")
+    public ResponseEntity<String> greetingText(@PathVariable String name) {
+    	
+    	Usuario usuario = new Usuario(null, "João Victor", 21);
+    	
+    	Usuario save = usuarioRepository.save(usuario);
+    	
+        return ResponseEntity.ok().body("Curso Spring Boot API: " + save.getName() + "!");
+    }
+    
+    @GetMapping(value = "/olamundo/{name}")
+    public ResponseEntity<String> retornaOlaMund(@PathVariable String name) {
+    	return ResponseEntity.ok().body("Olá mundo: " + name);
     }
 }
---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -0,0 +1,75 @@
+package br.com.springboot.curso_jdev_treinamento.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+	private String name;
+	private Integer idade;
+
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String name, Integer idade) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.idade = idade;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
