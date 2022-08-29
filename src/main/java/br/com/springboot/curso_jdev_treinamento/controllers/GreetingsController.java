c1aaac7204b03e29c60f31c23d0a9d215499c990
joao victor <joaovictormdasilva676@gmail.com>
2022-08-29 16:00:08 -0300

    Criando o END-POINT para salvar no banco

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,13 +1,17 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
@@ -42,10 +46,18 @@ public class GreetingsController {
     	return ResponseEntity.ok().body("Olá mundo: " + name);
     }
     
-    @GetMapping(value = "listatodos")
+    @GetMapping(value = "/listatodos")
     @ResponseBody/* RETORNA PARA O CORPO DA RESPOSTA*/
     public ResponseEntity<List<Usuario>> listaUsuario() {
     	List<Usuario> listUsuario = usuarioRepository.findAll(); /* executa uma busca no DB */
     	return ResponseEntity.ok().body(listUsuario);/* lista em JSON */
+    }
+    
+    @PostMapping(value = "/salvar") /* MAPPEIA URL */
+    @ResponseBody /*DESCRIÇÃO DA RESPOSTA*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+    	Usuario obj = usuarioRepository.save(usuario);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
+    	return ResponseEntity.created(uri).body(obj);
     }
 }
