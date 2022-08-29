647fca5b6696de4de242b2f19758a4d60eb6c2b7
joao victor <joaovictormdasilva676@gmail.com>
2022-08-29 15:28:09 -0300

    Criando END-POINT de listar todos usuários

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,9 +1,12 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
@@ -27,7 +30,7 @@ public class GreetingsController {
     @GetMapping(value = "/mostranome/{name}")
     public ResponseEntity<String> greetingText(@PathVariable String name) {
     	
-    	Usuario usuario = new Usuario(null, "João Victor", 21);
+    	Usuario usuario = new Usuario(null, name, 21);
     	
     	Usuario save = usuarioRepository.save(usuario);
     	
@@ -37,5 +40,12 @@ public class GreetingsController {
     @GetMapping(value = "/olamundo/{name}")
     public ResponseEntity<String> retornaOlaMund(@PathVariable String name) {
     	return ResponseEntity.ok().body("Olá mundo: " + name);
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody/* RETORNA PARA O CORPO DA RESPOSTA*/
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+    	List<Usuario> listUsuario = usuarioRepository.findAll(); /* executa uma busca no DB */
+    	return ResponseEntity.ok().body(listUsuario);/* lista em JSON */
     }
 }
