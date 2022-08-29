665a40e465ba5f9c9b72a4c3bfa76caac15cfef3
joao victor <joaovictormdasilva676@gmail.com>
2022-08-29 16:11:54 -0300

    Criando END-POINT para DELETE do banco

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -59,5 +61,12 @@ public class GreetingsController {
     	Usuario obj = usuarioRepository.save(usuario);
     	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
     	return ResponseEntity.created(uri).body(obj);
+    }
+    
+    @DeleteMapping(value = "/delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long id) {
+    	usuarioRepository.deleteById(id);
+    	return ResponseEntity.ok().body("Usuario deletado com sucesso");
     }
 }
